593703bb83f7258615c6f67cd865c3bee8eb4370
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-12-14 01:12:11 +0100

    "Added basic authentication to API using a token-based solution"

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # API endpoints require authentication by default
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- products/views.py
+++ products/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 class ProductListView(APIView):
     def get(self, request):
@@ -19,3 +21,9 @@ class ProductListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_products(request):
+    # Your product logic here
+    return Response({"message": "Authenticated access to products"})    
